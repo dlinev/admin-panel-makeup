14f80f05c7237b18ba11d2eb368be391dfd41d9a
dlinev <dlinev@bk.ru>
2022-01-24 13:34:39 +0300

    Create pre-commit hook

--- index.js
+++ index.js
@@ -3,42 +3,59 @@
 let isOnline = true;
 
 const age = 10;
-function someName () {
+function someName() {
   {
-      {
-          alert(age)
-      }
+    {
+      alert(age);
+    }
   }
 }
 
-const userName = prompt('Ввведите имя');
-const userEmail = prompt('Ввведите почту');
-const userPassword = prompt('Ввведите пароль');
+const userName = prompt("Ввведите имя");
+const userEmail = prompt("Ввведите почту");
+const userPassword = prompt("Ввведите пароль");
 
 alert(`
     name: ${userName}, email: ${userEmail}, password: ${userPassword}
 `);
 
-
 if (age === 20) {
-  alert(20)
-} else if (age = 21) {
-  aler(20)
-} else if (age = 20) {
-  aler(22)
-} else if (age = 22) {
-  aler(23)
-} else if (age = 23) {
-  aler(24)
-} else if (age = 24) {
-  aler(25)
+  alert(20);
+} else if ((age = 21)) {
+  aler(20);
+} else if ((age = 20)) {
+  aler(22);
+} else if ((age = 22)) {
+  aler(23);
+} else if ((age = 23)) {
+  aler(24);
+} else if ((age = 24)) {
+  aler(25);
 }
 
-switch(age) {
-  case 20: { alert(20);  break;}
-  case 20: { alert(20);  break;}
-  case 22: { alert(20);  break;}
-  case 23: { alert(20);  break;}
-  case 24: { alert(20);  break;}
-  case 25: { alert(20);  break;}
+switch (age) {
+  case 20: {
+    alert(20);
+    break;
+  }
+  case 20: {
+    alert(20);
+    break;
+  }
+  case 22: {
+    alert(20);
+    break;
+  }
+  case 23: {
+    alert(20);
+    break;
+  }
+  case 24: {
+    alert(20);
+    break;
+  }
+  case 25: {
+    alert(20);
+    break;
+  }
 }
